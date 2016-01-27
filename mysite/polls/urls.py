77d81358eb92322c45f082e42da67790c7088a8c
bezannier <bezannier.matthis@hotmail.fr>
2016-01-27 14:52:10 +0100

    ajout urlconf d'authentification

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-
+	url('^', include('django.contrib.auth.urls'))
 	url(r'^$', views.IndexView.as_view(), name='index'),
 
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
