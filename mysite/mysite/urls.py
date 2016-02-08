1a22ee0efa2fee88cdb8b8abff5770bbc9f6282e
bezannier <bezannier.matthis@hotmail.fr>
2016-02-08 11:42:09 +0100

    ajout des pages html dans urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -25,4 +25,8 @@ urlpatterns = [
     url(r'^inscription', views.InscriptionView, name='inscription'),
     url(r'^deconnexion$', views.deconnexion, name='deconnexion'),
     url(r'^1', views.ProfilView, name='profil'),
+    url(r'^base', views.BaseView, name='base'),
+    url(r'^chat', views.ChatView, name='chat'),
+    url(r'^amis', views.AmisView, name='amis'),
+    url(r'^evenement', views.EvenementView, name='evenement'),
 ]
