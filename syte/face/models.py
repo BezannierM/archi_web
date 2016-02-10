cb9a26a2d85d8e559a06a7125ee810cffbea8b42
bezannier <bezannier.matthis@hotmail.fr>
2016-02-10 14:36:13 +0100

    modif des models

--- syte/face/models.py
+++ syte/face/models.py
@@ -1,7 +1,7 @@
 import datetime
-
+from django.views.generic import TemplateView
 from django.db import models
-from django.contrib.auth.models import User
+
 from django.utils import timezone
 # Create your models here.
 
